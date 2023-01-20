f70daa779e7244ef645287f11eed302aeb47f6d6
abhishek kumar chaurasia <123100328+abhishek252003@users.noreply.github.com>
2023-01-20 10:10:43 +0530

    Add files via upload
    adding of two number in c programming

---
+++ adding_of_two_number.c
@@ -0,0 +1,22 @@
+/*Write a Program to show the use to input (Scanf)/output (Printf) 
+statements and block structure of C-program by highlighting the 
+features of "stdio.h".*/
+#include<stdio.h>
+int main()
+{
+  //Write a program to add two numbers and display the sum.//  
+    int a,b;
+    printf("ading of two nyumberis \n" );
+    scanf("%d", &a );
+
+    printf("enter thhe second number\n");
+
+    scanf("%d", &b);
+
+    printf("sun of the number is \n %d", a+b);
+
+
+
+  return 0;  
+}
+
